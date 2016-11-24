e7348608cef625c711203e694c83c6b6ccd8aaea
Chris De Rouck <chris@2minds.be>
2016-11-24 12:01:17 +0100

    when visibility of a column is turned off and you come back to the table, labels will be incorrect

--- bp2/app/lib/AlbumsList.js
+++ bp2/app/lib/AlbumsList.js
@@ -4,14 +4,41 @@ new Tabular.Table({
     name: "AlbumsList",
     collection: Albums,
     columns: [
-        {data: "artNr", title: "Art.Nr", class: "col-md-1"},
-        {data: "artistName", title: "Artist", class: "col-md-3"},
-        {data: "albumTitle", title: "Title", class: "col-md-4"},
-        {data: "inStock", title: "In stock", class: "col-md-1", visible: false},
-        {data: "backOrdersPossible", title: "Backorders Possible", class: "col-md-1"},
-        {data: "price", title: "Price", class: "col-md-1"},
         {
-            tmpl: Meteor.isClient && Template.AlbumActionBtns, class: "col-md-1"
+            data: "artNr",
+            titleFn: () => "Art.Nr",
+            class: "col-md-1",
+            visible: false
+        },
+        {
+            data: "artistName",
+            titleFn: () => "Artist",
+            class: "col-md-3"
+        },
+        {
+            data: "albumTitle",
+            titleFn: () => "Title",
+            class: "col-md-4"
+        },
+        {
+            data: "inStock",
+            titleFn: () => "In stock",
+            class: "col-md-1",
+            visible: false
+        },
+        {
+            data: "backOrdersPossible",
+            titleFn: () => "Backorders Possible",
+            class: "col-md-1"
+        },
+        {
+            data: "price",
+            titleFn: () => "Price",
+            class: "col-md-1"
+        },
+        {
+            tmpl: Meteor.isClient && Template.AlbumActionBtns,
+            class: "col-md-1"
         }
 
     ],
